3a9af666871ea4b04be3a7f38ec9e6bed005d385
Reyvin2005 <161600359+Reyvin2005@users.noreply.github.com>
2024-11-14 19:20:12 +0800

    Update HotelDBDriver.java

--- HotelDBDriver.java
+++ HotelDBDriver.java
@@ -6,7 +6,6 @@ public class HotelManagementDriver {
         Scanner scanner = new Scanner(System.in);
 
         HotelManagement hotelManagement = new HotelManagement(scanner);
-        GuestManagement guestManagement = new GuestManagement(scanner);
         EmployeeManagement employeeManagement = new EmployeeManagement(scanner);
         ReservationManagement reservationManagement = new ReservationManagement(scanner);
         PaymentManagement paymentManagement = new PaymentManagement(scanner);
@@ -15,10 +14,9 @@ public class HotelManagementDriver {
         while (true) {
             System.out.println("\nMain Menu:");
             System.out.println("1. Hotel Management");
-            System.out.println("2. Guest Management");
-            System.out.println("3. Employee Management");
-            System.out.println("4. Reservation Management");
-            System.out.println("5. Payment Management");
+            System.out.println("2. Employee Management");
+            System.out.println("3. Reservation Management");
+            System.out.println("4. Payment Management");
             System.out.println("0. Exit");
 
             System.out.print("Choose an option: ");
@@ -27,10 +25,9 @@ public class HotelManagementDriver {
 
             switch (choice) {
                 case 1 -> hotelManagement.showMenu();
-                case 2 -> guestManagement.showMenu();
-                case 3 -> employeeManagement.showMenu();
-                case 4 -> reservationManagement.showMenu();
-                case 5 -> paymentManagement.showMenu();
+                case 2 -> employeeManagement.showMenu();
+                case 3 -> reservationManagement.showMenu();
+                case 4 -> paymentManagement.showMenu();
                 case 0 -> {
                     System.out.println("Exiting the system. Goodbye!");
                     scanner.close();
